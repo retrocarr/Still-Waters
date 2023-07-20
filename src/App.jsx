5eb754281e23dfc64cc80755a7b740eb7fb6ecb0
murtatha muhammad <f.major.major7th@gmail.com>
2023-07-20 10:19:33 +0300

    first commit logo and paragraph look nice but break on small screens when adding buttons

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss'
+
+function App() {
+  return <>
+    <header className='hero'>
+      <h1>LOGO</h1>
+      <div className='seperator' />
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae, iste suscipit! Voluptas ex ipsum doloremque dolor earum a perferendis quaerat.</p>
+      {/* <button className="curleyBtn about">About</button>
+      <button className="curleyBtn whatWeDo">What we do</button>
+      <button className="curleyBtn contactUs">Contact us</button> */}
+    </header>
+  </>
+}
+
+export default App
